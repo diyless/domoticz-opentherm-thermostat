c0c4ce0078bca9430b2c56050f1f1463339791e3
Shcherban <shcherban91@gmail.com>
2020-12-30 00:56:46 +0200

    Initial src and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Domoticz OpentTherm Thermostat
+
+This repo contains a sketch to build a simple OpenTherm Wi-Fi thermostat controlled by Domoticz using [ESP8266 Thermostat Shield](https://diyless.com/product/esp8266-thermostat-shield).
+More details on setup: coming soon on
+https://diyless.com/
+
+## Domoticz OpentTherm Thermostat Schematic
+![Domoticz OpentTherm Thermostat Schematic](https://diyless.com/blog/opentherm-sample/master-opentherm-shield-connection.webp)
+
+## License
+Copyright (c) 2020 [DIYLESS](http://diyless.com/). Licensed under the [MIT license](/LICENSE?raw=true).
---
+++ opentherm-domoticz/config.h
@@ -0,0 +1,20 @@
+// Your WiFi credentials.
+// Set password to "" for open networks.
+const char* ssid = "xxxxxx";
+const char* pass = "xxxxxx";
+
+// Your MQTT broker address and credentials
+const char* mqtt_server = "xxx.xxx.xxx.xxx";
+const char* mqtt_user = "xxxxxx";
+const char* mqtt_password = "xxxxxx";
+const int mqtt_port = 1883;
+
+// Master OpenTherm Shield pins configuration
+const int OT_IN_PIN = 4;  //for Arduino, 4 for ESP8266 (D2), 21 for ESP32
+const int OT_OUT_PIN = 5; //for Arduino, 5 for ESP8266 (D1), 22 for ESP32
+
+// Temperature sensor pin
+const int ROOM_TEMP_SENSOR_PIN = 14; //for Arduino, 14 for ESP8266 (D5), 18 for ESP32
+
+const char* OUT_TOPIC = "domoticz/out";
+const char* IN_TOPIC = "domoticz/in";
